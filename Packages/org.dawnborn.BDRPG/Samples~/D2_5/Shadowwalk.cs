fd552b1975c995798413a208b22c1484b3439a0f
Andrew C <lackhand@users.noreply.github.com>
2022-09-25 16:27:45 -0400

    Fit & finish: Footsteps
    Change the patter to be a bit more fun; scale it with the actual motion of the character; add an idle.

--- Packages/org.dawnborn.BDRPG/Samples~/D2_5/Shadowwalk.cs
+++ Packages/org.dawnborn.BDRPG/Samples~/D2_5/Shadowwalk.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using BDUtil;
 using BDUtil.Math;
@@ -7,40 +6,49 @@ using UnityEngine;
 
 namespace BDRPG
 {
-    [RequireComponent(typeof(Rigidbody2D))]
     public class Shadowwalk : MonoBehaviour
     {
-        public float PartThresh = 1f;
-        new Rigidbody2D rigidbody;
+        public float SpeedScale = .5f;
+        public float SpeedDecay = .5f;
+        public float PartThresh = 2.5f;
+        Vector3 LastFrame;
+
+        float Speed;
+        float Accum;
         new SpriteRenderer renderer;
+        Animator animator;
         readonly Deque<SpriteRenderer> trail = new();
+
 
         void Awake()
         {
-            rigidbody = GetComponent<Rigidbody2D>();
             renderer = GetComponentInChildren<SpriteRenderer>();
+            animator = renderer.GetComponent<Animator>();
         }
-        IEnumerator Start()
+        void Start()
         {
-            float accum = 0f;
-            while (true)
-            {
-                float speed = rigidbody.velocity.magnitude;
-                accum += Time.deltaTime * speed;
-                if (accum > PartThresh)
-                {
-                    accum = 0;
-                    SpriteRenderer footprint = trail.PopBack() ?? new GameObject("Trail").AddComponent<SpriteRenderer>();
-                    footprint.gameObject.SetActive(true);
-                    footprint.sprite = renderer.sprite;
-                    footprint.color = Color.Lerp(Color.gray * renderer.color, Color.blue, speed / 30f).WithA(.5f);
-                    footprint.transform.rotation = renderer.transform.rotation;
-                    footprint.transform.position = transform.position;
-                    // In case we die, we want our clones to go *later*...
-                    Coroutines.StartCoroutine(FadeFootprint(footprint));
-                }
-                yield return null;
-            }
+            LastFrame = transform.position;
+            Speed = 0f;
+        }
+        void Update()
+        {
+            var speed = ((transform.position - LastFrame) / Time.deltaTime).magnitude;
+            LastFrame = transform.position;
+            if (speed > 1e-06f) Speed = speed;
+            else Speed *= SpeedDecay;
+            animator.SetFloat("Speed", Speed * SpeedScale);
+            if (Speed < PartThresh) { Accum = 0; return; }
+            Accum += Time.deltaTime * Speed;
+            if (Accum < PartThresh) return;
+            Accum = 0;
+            SpriteRenderer footprint = trail.PopBack() ?? new GameObject("Trail").AddComponent<SpriteRenderer>();
+            footprint.gameObject.SetActive(true);
+            footprint.sprite = renderer.sprite;
+            footprint.color = Color.Lerp(Color.gray * renderer.color, Color.blue, Speed / 5f).WithA(.25f);
+            footprint.transform.rotation = renderer.transform.rotation;
+            footprint.transform.position = transform.position + .0001f * new Vector3(1f, 1f, 0f);
+            // In case we die, we want our clones to go *later*...
+            Coroutines.StartCoroutine(FadeFootprint(footprint));
         }
         IEnumerator FadeFootprint(SpriteRenderer footprint)
         {
